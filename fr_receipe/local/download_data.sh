0e07e4515a94754e19ef52c999bbc6c3fe368d44
Corentin Mounier <mounie_c@epita.fr>
2019-09-24 17:18:53 +0200

    fr receipe: add scripts

---
+++ fr_receipe/local/download_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright  2014  Nickolay V. Shmyrev
+#            2014  Brno University of Technology (Author: Karel Vesely)
+#            2016  John Hopkins University (author: Daniel Povey)
+# Apache 2.0
+
+mkdir -p db
+
+cd db  ### Note: the rest of this script is executed from the directory 'db'.
+
+# DECODA database:
+# deleted: DECODA database already existe
+
+exit 0
+
