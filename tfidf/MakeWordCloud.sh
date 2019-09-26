d18e0d5446b7c4623b9fb3ca0867305c3cda75f0
sidore_m <marc@sidorenko.fr>
2019-09-26 18:46:34 +0200

    WordCloud: fix scripts and size of images words

--- tfidf/MakeWordCloud.sh
+++ tfidf/MakeWordCloud.sh
@@ -4,9 +4,13 @@ echo "Make Word Cloud"
 echo "--"
 echo "Use of tf_result"
 
-echo "sort -rh tf_result | head -n 50 > word_cloud_results.wcdl"
-sort -rh tf_result | head -n 50 > word_cloud_results.wcdl
+echo "sort -rh tf_result | head -n 120 > word_cloud_results.wcdl"
+sort -rh tf_result | head -n 120 > word_cloud_results.wcdl
 
 echo "tr ' ' '\t' < word_cloud_results.wcdl > word_cloud_end.wcdl"
 tr ' ' '\t' < word_cloud_results.wcdl > word_cloud_end.wcdl
+
+echo "Simply number"
+sed "s/[0-9][0-9][0-9]\t/\t/g" word_cloud_end.wcdl | head -n 120 >  wordcloud_STM_FR.wcld
+
 
--- tfidf/MegaScriptCTMToTF.sh
+++ tfidf/MegaScriptCTMToTF.sh
@@ -4,8 +4,8 @@ echo 'cat ../exp/tri3/decode_test/scor*/ctm | sed -E "s/.*_|\d|A|U|P|\.//g" | se
 cat ../exp/tri3/decode_test/scor*/ctm | sed -E "s/.*_|\d|A|U|P|\.//g" | sed -E "s/[0-9]//g" | iconv -f ISO-8859-1 -t UTF-8 | ./CompterMotsHachage.pl > ctm_tf_results
 
 
-echo 'sed \"s/.[0-9][0-9][0-9] /. /g\" ctm_tf_results | head -n 100 >  worldcloud_CTM_EN.wcld'
-sed "s/.[0-9][0-9][0-9] /. /g" ctm_tf_results | head -n 100 >  worldcloud_CTM_EN.wcld
+echo 'sed \"s/[0-9][0-9][0-9] / /g\" ctm_tf_results | head -n 100 >  worldcloud_CTM_EN.wcld'
+sed "s/[0-9][0-9][0-9] / /g" ctm_tf_results | head -n 100 >  wordcloud_CTM_EN.wcld
 
 
 
